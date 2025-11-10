5faf18b28c05dbc03836e044c0d8bd952c41c9a9
vladimirov_m <vladimirov_m@tochka.com>
2025-11-10 21:45:36 +0500

    fix: fix CORS

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from src.api.ping import router as ping_router
 from src.api.users import router as users_router
 from src.api.auth import router as auth_router
@@ -18,6 +20,19 @@ app = FastAPI(title="UDV Team Map API", lifespan=lifespan)
 app.include_router(ping_router, prefix="/api", tags=["ping"])
 app.include_router(users_router, prefix="/api", tags=["users"])
 app.include_router(auth_router, prefix="/api", tags=["auth"])
+
+origins = [
+    "http://localhost:3000",   # ваш фронтенд dev URL
+    # "https://your.production.frontend"  # production origin, если нужно
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           # нельзя ставить ["*"] если allow_credentials=True
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
