f7d6777dab0693eed3337aed8274c30b8032abbf
vladimirov_m <vladimirov_m@tochka.com>
2025-12-19 03:08:46 +0500

    chore: remove ad_users

--- src/application/services/__init__.py
+++ src/application/services/__init__.py
@@ -1,6 +1,5 @@
 from .user import UserService
-from .ad_users import ADUser
 from .ad_import import AdImportService
 from .avatar import AvatarService
 
-__all__ = ['UserService', "ADUser", "AdImportService", "AvatarService"]
+__all__ = ['UserService', "AdImportService", "AvatarService"]
